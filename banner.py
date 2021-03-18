c387d0906ef0561fea9b8525c1e6b9e11735c9ff
danguya <daniel.yava16@gmail.com>
2021-03-18 22:18:05 +0000

    ADD Clean Code, add module requests and args_parse

---
+++ banner.py
@@ -0,0 +1,5 @@
+def banner():
+    print("[ DeepWeb Find Link -> Scappre the deepweb to find links")
+    print("[+] Made by: Kurogai")
+    print("[Github] -> https://github.com/kurogai")
+    print("\n\n")
